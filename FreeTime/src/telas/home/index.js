bc21b04ace565cc4d08d96e05314e4b753c7dd7a
LSeraphim <leostart133@gmail.com>
2024-05-27 20:40:36 -0300

    Adicionado codigo do bd SQL, pasta backend com servidor.js fazendo conexao

---
+++ FreeTime/src/telas/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Inicio() {
+    const navigation = useNavigation();
+    return (
+        <View>
+            
+        </View>
+    )
+
+}
