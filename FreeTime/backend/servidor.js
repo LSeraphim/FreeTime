ec1445caec8fcd24d51c0bd920a37fb9ae7f9bd1
LSeraphim <leostart133@gmail.com>
2024-05-28 04:12:39 -0300

    Adicionado conexao com servidor

--- FreeTime/backend/servidor.js
+++ FreeTime/backend/servidor.js
@@ -20,23 +20,3 @@ db.connect((err) => {
     }
     console.log('Conexão bem-sucedida ao banco de dados MySQL')
 });
-
-// Rota para Inserir um novo usuário
-
-app.post('/registro', (req, res) => {
-    const {email, senha, nomeCompleto, nomeUsuario, emailGoogle} = req.body;
-    const sql = `INSERT INTO ContaUsuario (Email, Senha, NomeCompleto, NomeUsuario, EmailGoogle, Foto) VALUES (?, ?, ?, ?, ?, ?)`;
-    db.query(sql, [email, senha, nomeCompleto, nomeUsuario, emailGoogle], (err, result) => {
-        if (err) {
-            console.error('Erro ao inserir usuario', err);
-            res.status(500).send('Erro interno do servidor');
-            return;
-        }
-        console.log('Usuário inserido com sucesso');
-        res.status(200).send('Usuário registrado com sucesso');
-    });
-});
-
-app.listen(port, () => {
-console.log(`Servidor backend esta rodando em http://localhost:${port}`);
-});
--- FreeTime/src/telas/Registro/index.js
+++ FreeTime/src/telas/Registro/index.js
@@ -1,140 +1,124 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, SafeAreaView, TouchableOpacity  } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useState } from 'react';
 
 import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
 
 export default function Registro() {
-  const navigation = useNavigation();
-  const [dadosRegistro, setDadosRegistro] = useState({
-    email: '',
-    senha: '',
-    nomeCompleto: '',
-    nomeUsuario: '',
-    emailGoogle: '',
-  });
 
-  const registrarUsuario = async () => {
-    try {
-      const response = await fetch('https://192.168.77.81:8081/Registro', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dadosRegistro)
-      });
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
 
-      if (response.ok) {
-        alert('Usuário registrado com sucesso');
-        // Redirecionar para a próxima tela após o registro bem-sucedido
-        navigation.navigate('Login');
-      } else {
-        throw new Error('Erro ao registrar usuário');
-      }
-    } catch (error) {
-      console.error('Erro ao registrar usuário:', error.message);
-      alert('Erro ao registrar usuário. Por favor, tente novamente mais tarde.');
-    }
-  };
+      {/* Container do header com "Register" */}
+      <View style={styles.headerContainer}>
+        <Text style={{ fontSize: 32, color: '#ffff', textAlign: 'center' }}>Registrar-se</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <AntDesign name='back' color={'#fff'} size={25} />
+        </TouchableOpacity>
+      </View>
 
-    return (
-        <SafeAreaView style={styles.container}>
-          { <StatusBar style="auto" /> }
-          
+      {/* Dados do usuário */}
+      <Animatable.View delay={100} animation="bounceInLeft" style={styles.containerForm}>
+        <View style={styles.inputContainer}>
+          <Text style={{ fontSize: 16 }}>Nome Completo</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              onChangeText={""}
+              placeholder='Seu nome completo'
+            />
+          </View>
 
-            {/* Container do header com "Register" */}
-            <View style={styles.headerContainer}>
-              <Text style={{fontSize:32, color:'#ffff', textAlign:'center'}}>Registrar-se</Text>
-              <TouchableOpacity
-              onPress={ () => navigation.navigate('Login')}>
-                <AntDesign name='back' color={'#fff'} size={25} />
-              </TouchableOpacity>
-            </View>
+          <Text style={{ fontSize: 16 }}>Usuário</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              onChangeText={""}
+              placeholder='Seu nome de usuário'
+            />
+          </View>
 
-            {/* Dados do usuário */}
-          <Animatable.View delay={100} animation="bounceInLeft" style={styles.containerForm}>
-            <View style={styles.inputContainer}>
-              <Text style={{fontSize:16}}>Nome Completo</Text>
-              <View style={styles.inputBox}>
-                <TextInput  onChangeText={""} placeholder='Seu nome completo'/>
-              </View>
+          <Text style={{ fontSize: 16 }}>E-mail</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              onChangeText={""}
+              placeholder='Seu E-mail'
+            />
+          </View>
 
-              <Text style={{fontSize:16}}>Usuário</Text>
-              <View style={styles.inputBox}>
-                <TextInput  onChangeText={""} placeholder='Seu nome de usuário'/>
-              </View>
+          <Text style={{ fontSize: 16 }}>Senha</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              onChangeText={""}
+              placeholder='Sua senha'
+              secureTextEntry
+            />
+          </View>
 
-              <Text style={{fontSize:16}}>E-mail</Text>
-              <View style={styles.inputBox}>
-                <TextInput  onChangeText={""} placeholder='Seu E-mail'/>
-              </View>
-
-              <Text style={{fontSize:16}}>Senha</Text>
-              <View style={styles.inputBox}>
-                <TextInput  onChangeText={""} placeholder='Sua senha'/>
-              </View>
-
-              <Text style={{fontSize:16}}>Confirmar Senha</Text>
-              <View style={styles.inputBox}>
-                <TextInput  onChangeText={""} placeholder='Confirme a senha'/>
-              </View>
-            </View>    
-              {/* Button */}
-            <TouchableOpacity onPress={registrarUsuario}>
-                <View style={styles.containerButton}>
-                    <Text style={{color: '#ffff', fontSize: 24}}>Registrar-se</Text>
-                </View>
-            </TouchableOpacity>
-          </Animatable.View>
-        </SafeAreaView>
-    );
-};
+          <Text style={{ fontSize: 16 }}>Confirmar Senha</Text>
+          <View style={styles.inputBox}>
+            <TextInput
+              onChangeText={""}
+              placeholder='Confirme a senha'
+              secureTextEntry
+            />
+          </View>
+        </View>
+        {/* Button */}
+        <TouchableOpacity onPress={registrarUsuario}>
+          <View style={styles.containerButton}>
+            <Text style={{ color: '#ffff', fontSize: 24 }}>Registrar-se</Text>
+          </View>
+        </TouchableOpacity>
+      </Animatable.View>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#04777D',
-        flex:1
-    },
-    headerContainer: {
-      flex:1,
-      backgroundColor: '#04777D',
-      justifyContent: 'center',
-      paddingStart: '5%',
-      paddingEnd: '5%',
-    },
-    userContainer: {
-        paddingTop: 20,
-        width: '80%',
-    },
-    inputContainer:{
-      width: '100%'
-    },
-    inputBox: {
-      width: '100%',
-      borderBottomColor: '#020202',
-      borderBottomWidth: 1,
-      marginBottom: 40
-    },
-    containerButton: {
-      backgroundColor: 'rgba(4, 119, 125, 1)',
-      width: 226,
-      height: 73,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 60,
-      marginBottom: 20
-    },
-    containerForm:{
-      flex:4,
-      backgroundColor:'#fff',
-      borderTopLeftRadius: 60,
-      borderTopRightRadius: 60,
-      paddingStart: '10%',
-      paddingEnd: '10%',
-      alignItems: 'center',
-      textAlign: 'left',
-      paddingTop: 50,
-  }
+  container: {
+    backgroundColor: '#04777D',
+    flex: 1,
+  },
+  headerContainer: {
+    flex: 1,
+    backgroundColor: '#04777D',
+    justifyContent: 'center',
+    paddingStart: '5%',
+    paddingEnd: '5%',
+  },
+  userContainer: {
+    paddingTop: 20,
+    width: '80%',
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  inputBox: {
+    width: '100%',
+    borderBottomColor: '#020202',
+    borderBottomWidth: 1,
+    marginBottom: 40,
+  },
+  containerButton: {
+    backgroundColor: 'rgba(4, 119, 125, 1)',
+    width: 226,
+    height: 73,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 60,
+    marginBottom: 20,
+  },
+  containerForm: {
+    flex: 4,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+    paddingStart: '10%',
+    paddingEnd: '10%',
+    alignItems: 'center',
+    textAlign: 'left',
+    paddingTop: 50,
+  },
 });
